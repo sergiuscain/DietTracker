df8c56d2f49f7c12e9398b4553ed9ca099bc3cf2
Sergey Korolev <sergiussmithe@gmail.com>
2026-06-21 21:10:58 +0300

    Реализовал метод AddMealEntry в JsonStorageModule.

---
+++ Diet Tracker/Modules/JsonStorageModule.cs
@@ -0,0 +1,61 @@
+﻿using System.Text.Json;
+using Diet_Tracker.Models;
+
+namespace Diet_Tracker.Modules;
+
+internal class JsonStorageModule : IStorageModule
+{
+    private string filePath = "MealData.json";
+    public bool AddMealEntry(MealEntry mealEntry)
+    {
+        try
+        {
+            if (File.Exists(filePath))
+            {
+                var lastJsonData = File.ReadAllText(filePath);
+                var data = JsonSerializer.Deserialize<ICollection<MealEntry>>(lastJsonData);
+                data?.Add(mealEntry);
+                var newJsonData = JsonSerializer.Serialize(data);
+                File.WriteAllText(filePath, newJsonData);
+            }
+            else
+            {
+                var data = new List<MealEntry>();
+                data.Add(mealEntry);
+                var jsonData = JsonSerializer.Serialize(data);
+                File.WriteAllText(filePath, jsonData);
+            }
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Ошибка при записи данных: {ex.ToString()}");
+            return false;
+        }
+        return true;
+    }
+
+    public ICollection<MealEntry> GetAllMealEntries()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ICollection<MealEntry> GetAllMealEntries(int page, int pageSize)
+    {
+        throw new NotImplementedException();
+    }
+
+    public MealEntry GetMealEntry(int mealEntryId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool RemoveMealEntry(int mealEntryId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public MealEntry UpdateMealEntry(MealEntry mealEntry, int mealEntryId)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Diet Tracker/Program.cs
+++ Diet Tracker/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Diet_Tracker.Models;
+using Diet_Tracker.Modules;
+
+Console.WriteLine("Hello, World!");
+var x = new JsonStorageModule();
+var meal = new MealEntry
+{
+    Calories = 100,
+    DateTime = DateTime.Now,
+    Carbohydrates = 100,
+    Fats = 100,
+    FoodName = "Макарошки с сосиской",
+    Id = 0,
+    MealType = MealType.breakfast,
+    PortionWeight = 100,
+    Proteins = 999
+};
+x.AddMealEntry(meal);
