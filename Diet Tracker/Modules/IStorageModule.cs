19c7c21d143c624ad097681ad58aa131084e3870
Sergey Korolev <sergiussmithe@gmail.com>
2026-06-21 20:26:01 +0300

    Реализовал интерфейс для работы с хранилищем данных (база данных и/или другой тип хранения данных. Например, в JSON формате в текстовом файле)

---
+++ Diet Tracker/Modules/IStorageModule.cs
@@ -0,0 +1,43 @@
+﻿using Diet_Tracker.Models;
+
+namespace Diet_Tracker.Modules;
+internal interface IStorageModule
+{
+    /// <summary>
+    /// Добавляет запись приёма пищи
+    /// </summary>
+    /// <param name="mealEntry">Модель приёма пищи</param>
+    /// <returns></returns>
+    public bool AddMealEntry(MealEntry mealEntry);
+    /// <summary>
+    /// Удаляет запись приёма пищи
+    /// </summary>
+    /// <param name="mealEntryId">Id записи</param>
+    /// <returns></returns>
+    public bool RemoveMealEntry(int mealEntryId);
+    /// <summary>
+    /// Получает запись приёма пищи по Id
+    /// </summary>
+    /// <param name="mealEntryId">Id записи</param>
+    /// <returns></returns>
+    public MealEntry GetMealEntry(int mealEntryId);
+    /// <summary>
+    /// Получает все записи приемов пищи
+    /// </summary>
+    /// <returns></returns>
+    public ICollection<MealEntry> GetAllMealEntries();
+    /// <summary>
+    /// Перегрузка получения записей приёмов пищи с пагинацией
+    /// </summary>
+    /// <param name="page">Номер страницы. Отчёт с нулевой начинается</param>
+    /// <param name="pageSize">Размер страницы</param>
+    /// <returns></returns>
+    public ICollection<MealEntry> GetAllMealEntries(int page, int pageSize);
+    /// <summary>
+    /// Обновление записи с указанным Id
+    /// </summary>
+    /// <param name="mealEntry">Новая запись</param>
+    /// <param name="mealEntryId">Id записи, которую необходимо обновить</param>
+    /// <returns></returns>
+    public MealEntry UpdateMealEntry(MealEntry mealEntry, int mealEntryId);
+}
