e5bc5965e8fd9c4ba352cd6b8e77aeadb09d0459
Sergey Korolev <sergiussmithe@gmail.com>
2026-06-23 17:40:38 +0300

    Создал класс для отчетности и класс модели данных заполнения целей КБЖУ

---
+++ Diet Tracker/Models/NutrientGoalReport.cs
@@ -0,0 +1,12 @@
+﻿namespace Diet_Tracker.Models;
+internal class NutrientGoalReport
+{
+    public int CurrentCalories {  get; set; }
+    public int CurrentProteins { get; set; }
+    public int CurrentFats { get; set; }
+    public int CurrentCarbohydrates { get; set; }
+    public int GoalCalories { get; set; }
+    public int GoalProteins { get; set; }
+    public int GoalFats { get; set; }
+    public int GoalCarbohydrates { get; set; }
+}
---
+++ Diet Tracker/Modules/ReportModule.cs
@@ -0,0 +1,22 @@
+﻿using Diet_Tracker.Models;
+
+namespace Diet_Tracker.Modules;
+internal class ReportModule
+{
+    /// <summary>
+    /// Ежедневный отчёт
+    /// </summary>
+    /// <returns></returns>
+    public ICollection<MealEntry> DailyReport()
+    {
+        return new List<MealEntry>();
+    }
+    public ICollection<MealEntry> PeriodReport()
+    {
+        return new List<MealEntry>();
+    }
+    public NutrientGoalReport GetNutrientGoalReport(DateTime date)
+    {
+        return new NutrientGoalReport();
+    }
+}
